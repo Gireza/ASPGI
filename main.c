a91a341d74ad1c040e0bfbac35248addccddd1d0
Hector-Junior <82316857+Hector-Junior@users.noreply.github.com>
2022-04-14 14:51:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,9 +7,20 @@
 
 int main(void){
    
-    /*
-    
-   parse();
-   saturation();
-*/
+      FILE *fichier = NULL;
+    fichier = fopen("fichier.txt", "r+"); // r+ pour lire et écrire !
+
+    if (fichier == NULL) // On ne trouve pas le fichier...
+    {
+        printf("Impossible de trouver le fichier \"fichier.txt\"\n");
+        return 0;
+        // TODO : gestion des erreurs...
+    }
+    unsigned int rkMax[0] = {};
+    unsigned int rkMin[0] = {};
+    int n_points = 0;
+   
+   parse(fichier,rkMin,rkMax,n_points);
+   saturation(rkMin,rkMax,n_points);
+
 }
