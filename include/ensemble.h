4f81c5c4768469eab5f1ac03f6bfbaf9e0985d54
Hector-Junior <82316857+Hector-Junior@users.noreply.github.com>
2022-04-14 13:49:50 +0200

    Update ensemble.h

--- include/ensemble.h
+++ include/ensemble.h
@@ -1,13 +1,14 @@
 #ifndef ENSEMBLE_H
 #define ENSEMBLE_H
 
+
 int unionPratique(int x,int y);
 int interPratique(int x,int y);
 int puissance(int puiss);
 int logarithme(int x);
 int appartienPratique(int tenir, int appartien);
 int cardinal(int indiceEnsemble); 
-int parse(FILE * file, int** rkMin, int** rkMax, int* n_points);
+
 
 
 #endif //ENSEMBLE_H
